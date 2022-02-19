ded0ae025bb6115b6cdfa4e196c03f29de2ed0f7
Faisal Hossain <faisalhossain@s172-20-14-h188.paws.uga.edu>
2022-02-19 13:53:18 -0500

    sign up page

---
+++ client/src/stockEval.js
@@ -0,0 +1,20 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './App.css';
+
+const auth = null;
+const user = null
+
+function stockEval() {
+  return (
+    <div className="App">
+        <Button variant="text" onClick={signInWithGoogle}>
+        Click me
+        </Button>
+        <h1></h1>
+    </div>
+  );
+}
+
+
+export default stockEval;
