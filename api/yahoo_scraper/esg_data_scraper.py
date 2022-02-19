e24db713fbf55137a6fca6ce8566c9e9352e412f
ayush-kumar-1 <58648531+ayush-kumar-1@users.noreply.github.com>
2022-02-19 10:53:05 -0500

    added combined stocks file:

--- api/yahoo_scraper/esg_data_scraper.py
+++ api/yahoo_scraper/esg_data_scraper.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import re
+import glob
 
 from time import sleep
 from selenium import webdriver
@@ -12,13 +13,18 @@ from selenium.common.exceptions import NoSuchElementException
 
 def main(): 
     stocks = pd.read_csv("nasdaq.csv")
-    symbols = list(stocks.sort_values(by = "Market Cap", ascending = False)["Symbol"][:200])\
+
+    symbols = glob.glob("stocks/*.csv")
+    symbols = [item[7: item.find(".csv")] for item in symbols]
 
     df = pd.DataFrame({"symbols": symbols})
     df["link"] = df.apply(lambda row: f"https://www.morningstar.com/stocks/xnas/{row.symbols}/sustainability", axis = 1)
     
     ser = Service("D:/ugahacks7/api/yahoo_scraper/chromedriver_win32/chromedriver.exe")
     driver = webdriver.Chrome(service = ser)
+
+    done = pd.read_csv("sustainability_backup.csv")["symbols"].to_list()
+    df = df[~df.symbols.isin(done)]
 
     df["data"] = ""
     for i, row in df.iterrows(): 
@@ -38,9 +44,9 @@ def main():
         df.loc[i, "data"] = (issues, score, incidents)
 
         if i % 5 == 0:
-            df.to_csv("sustainability_backup.csv", index = False)
+            df.to_csv("sustainability_backup.csv", index = False, mode = 'a')
 
-    df.to_csv("sustainability.csv", index = False)
+    df.to_csv("sustainability.csv", index = False, mode = "a")
 
 
 if __name__ == "__main__": 
