dae4375014df633a3f13d4339f57d800f5a3bfc2
charanr0121 <charanr0121@gmail.com>
2022-02-19 18:51:52 -0500

    CHARTS

--- client/src/Stock.js
+++ client/src/Stock.js
@@ -1,11 +1,12 @@
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Iframe from 'react-iframe'
-
+// import Button from '@mui/material/Button';
+// import Grid from '@mui/material/Grid';
+// import Iframe from 'react-iframe'
+import axios from 'axios';
+import React from "react";
 
 // import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 // import Grid from '@mui/material/Grid';
 // import Button from '@mui/material/Button';
 
@@ -15,13 +16,31 @@ import './App.css';
 const auth = null;
 const user = null
 
-function Stock() {
-  return (
-    <div className="App">
-      <Iframe width="1000" height="500" url="https://datastudio.google.com/embed/reporting/916064de-74e2-4d73-ab71-b0603b6399d1/page/p_k5sn4dkcsc?params=%7B%22df7%22:%22include%25EE%2580%25800%25EE%2580%2580IN%25EE%2580%2580GOOG%22%7D" frameborder="0" style="border:0" allowfullscreen>d</Iframe>
-    </div>
-  );
+export default class Stock extends React.Component{
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/stock/goog`)
+      .then(res => {
+        const data = res.data;
+        this.setState({ data });
+        for (var i = 0; i < data.length; i++){
+          console.log(data[i])
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.data}
+        {/* <Iframe width="1000" height="500" url="https://datastudio.google.com/embed/reporting/916064de-74e2-4d73-ab71-b0603b6399d1/page/p_k5sn4dkcsc?params=%7B%22df7%22:%22include%25EE%2580%25800%25EE%2580%2580IN%25EE%2580%2580GOOG%22%7D" frameborder="0" style="border:0" allowfullscreen>d</Iframe> */}
+      </div>
+    );
+  }
 }
 
 
-export default Stock;
+// export default Stock;
