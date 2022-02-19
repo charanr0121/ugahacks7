2f453c01669242e0cf7cd36e58acc88c350d9444
VictorElujinmi <57678101+VictorElujinmi@users.noreply.github.com>
2022-02-19 14:40:33 -0500

    fix

--- client/src/Values.js
+++ client/src/Values.js
@@ -7,40 +7,6 @@ import { shadows } from '@mui/system';
 
 import './App.css';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      bgColor: ""
-    }
-  }
-
-
-  boxClick = (e) => {
-    this.setState({
-      bgColor: "red"
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-
-
-        <article className='experimentsHolder'>
-          <h2>Test 3</h2>
-          <p>This is an example of an onClick event 'renderd' by react.</p>
-          <div className="boxClickCss"
-            style={{ backgroundColor: this.state.bgColor }}
-            onClick={this.boxClick}>Click Me!</div>
-        </article>
-
-      </div>
-    );
-  }
-}
-
 
 function changeButtonColor(button) {
   console.log("Hello");
@@ -58,11 +24,6 @@ function Values() {
         sx={{ p: 30, backgroundColor: 'white'}}
         >
 
-        <Grid item style={{ width: "40%", height: "100%"}} sx={{ backgroundColor: 'blue' }}>
-          <Grid item sx={{ p: 1 }}>
-            <Button variant="outlined" size="large" style={{ width: "100%" }} onClick={changeButtonColor(this)} > Option 1 </Button>
-          </Grid>
-        </Grid>  
           
 
         <Grid item style={{ width: "50%" }}>
@@ -94,7 +55,6 @@ function Values() {
     </div>
   );
 }
-
 
 
 
