deb985bdfb3c66a2625e67005d8c0d43b398780e
charanr0121 <charanr0121@gmail.com>
2022-02-20 01:49:32 -0500

    SSS

--- client/src/Values.js
+++ client/src/Values.js
@@ -1,10 +1,12 @@
-import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import FormGroup from '@mui/material/Grid';
+
 import Button from '@mui/material/Button';
 import * as React from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Lottie from 'react-lottie';
 import animationData from './lotties/lf20_q9mzugao.json';
 import { red } from '@mui/material/colors';
@@ -34,6 +36,11 @@ function StandaloneToggleButton(props) {
 }
 
 function Values() {
+
+  const [checkedState, setCheckedState] = useState(
+    new Array(16).fill(false)
+  );
+
   return (
     <div className="App" sx={{ p: 0, height: "100%", width: "100%",   }}>
         <Grid 
@@ -64,29 +71,28 @@ function Values() {
           justifyContent="center"
 
         >
-          <Grid item sx={{ p: 1 }}>
-            <StandaloneToggleButton esg="Business Ethics"></StandaloneToggleButton>
-            {/* <Button variant="outlined" size="large" style={{ width: "100%" }} onClick={changeButtonColor(this)} > Option 1 </Button> */}
-          </Grid>
-
-          <Grid item sx={{ p: 1 }}>
-            <StandaloneToggleButton esg="Socail">HHH</StandaloneToggleButton>
-          </Grid>
-
-          <Grid item sx={{ p: 1 }}>
-            <StandaloneToggleButton esg="ENV">HHH</StandaloneToggleButton>
-          </Grid>
-
-          <Grid item sx={{ p: 1 }}>
-            <StandaloneToggleButton esg="media">HHH</StandaloneToggleButton>
-          </Grid>
-          <Grid item sx={{ p: 1 }}>
-            <StandaloneToggleButton esg="?">HHH</StandaloneToggleButton>
-          </Grid>
-
-          <Grid item sx={{ p: 1 }}>
-            <StandaloneToggleButton esg="??">HHH</StandaloneToggleButton>
-          </Grid>
+          <form onSubmit={handleSubmit}>
+            <FormGroup >
+              <Grid container spacing={2} columns={12}   justifyContent="space-between">
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Ethical Busines" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Education" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Data Privacy" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Following Regulation" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Anti-pollution" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Equity" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Sustainability" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Transparency" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Carbon Footprint" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Worker Rights & Protection" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Anti-corruption" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Environmental Impact" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Human Rights" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Supplier Sustainability" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Land Preservation" /> </Grid>
+                <Grid item xs={"auto"}> <FormControlLabel control={<Checkbox  />} label="Sustainable Products" /></Grid>
+              </Grid>
+            </FormGroup>
+          </form>
 
         </Grid>
 
